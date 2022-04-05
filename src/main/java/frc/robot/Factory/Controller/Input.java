bb2f26f3377d2106eddce2740da9bcb683909cfb
bossmaster217 <92772910+bossmaster217@users.noreply.github.com>
2022-04-04 18:41:32 -0700

    changed naming and added mechanisms and drive

--- src/main/java/frc/robot/Factory/Controller/Input.java
+++ src/main/java/frc/robot/Factory/Controller/Input.java
@@ -7,6 +7,7 @@ package frc.robot.Factory.Controller;
 import com.ctre.phoenix.motorcontrol.ControlMode;
 
 import edu.wpi.first.wpilibj.XboxController;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import frc.robot.Constants.CONTROLLERS.ShootInput;
 import frc.robot.Constants.CONTROLLERS.ClimbInput;
 import frc.robot.Constants.CONTROLLERS.DriveInput;
@@ -16,11 +17,13 @@ import frc.robot.Constants.CLIMBER;
 import frc.robot.Constants.CONTROLLERS;
 import frc.robot.Constants.ELEVATOR;
 import frc.robot.Constants.INTAKE;
+import frc.robot.Constants.MKTRAIN;
 import frc.robot.Constants.SHOOTER;
 import frc.robot.Factory.Controller.MkXboxInput.Type;
 import frc.robot.Mechanisims.Climber;
 import frc.robot.Mechanisims.Elevator;
 import frc.robot.Mechanisims.Intake;
+import frc.robot.Mechanisims.MkSwerveTrain;
 import frc.robot.Mechanisims.Shooter;
 import frc.robot.ToolShed.SwerveAlgorithims;
 import frc.robot.wpi.Odometry;
@@ -28,22 +31,22 @@ import frc.robot.wpi.Odometry;
 /** Add your docs here. */
 public class Input {
     private XboxController xbox = new XboxController(0);
+    private XboxController op = new XboxController(1);
     private MkXboxInput fakeLimelightInput = new MkXboxInput(xbox, CONTROLLERS.fakeLimelight, Type.Button, false);
     private MkXboxInput[] driveInput = {new MkXboxInput(xbox, DriveInput.fwd, Type.Axis, false, 0.1), new MkXboxInput(xbox, DriveInput.str, Type.Axis, false, 0.1), new MkXboxInput(xbox, DriveInput.rcw, Type.Axis, false, 0.1)};
-    private MkXboxInput intakeInput = new MkXboxInput(xbox, IntakeInput.intake, Type.Button, true);
-    private MkXboxInput rollerInput = new MkXboxInput(xbox, IntakeInput.roller, Type.Axis, false, 0.1);
-    private MkXboxInput[] shooterInput = {new MkXboxInput(xbox, ShootInput.forwardShoot, Type.Axis, false, 0.1), new MkXboxInput(xbox, ShootInput.backwardShoot, Type.Axis, false, 0.1)};
-    private MkXboxInput[] climberLeftInput = {new MkXboxInput(xbox, ClimbInput.leftClimbUp, Type.Button, false), new MkXboxInput(xbox, ClimbInput.leftClimbDown, Type.Button, false)};
-    private MkXboxInput[] climberRightInput = {new MkXboxInput(xbox, ClimbInput.rightClimbUp, Type.Button, false), new MkXboxInput(xbox, ClimbInput.rightClimbDown, Type.Button, false)};
-    private MkXboxInput climbInput = new MkXboxInput(xbox, ClimbInput.climb, Type.Axis, false);
-    private MkXboxInput autoClimbInput = new MkXboxInput(xbox, ClimbInput.autoClimb, Type.Button, true);
-    private MkXboxInput elevatorInput = new MkXboxInput(xbox, ElevatorInput.elevator, Type.Axis, false, 0.1);
+    private MkXboxInput intakeInput = new MkXboxInput(op, IntakeInput.intakeButton, Type.Button, true);
+    private MkXboxInput rollerInput[] = {new MkXboxInput(op, IntakeInput.rollerForwardBumper, Type.Axis, false, 0.1), new MkXboxInput(op, IntakeInput.rollerBackwardBumper, Type.Axis, false, 0.1)};
+    private MkXboxInput[] shooterInput = {new MkXboxInput(op, ShootInput.forwardShootTrigger, Type.Axis, false, 0.1), new MkXboxInput(op, ShootInput.backwardShootTrigger, Type.Axis, false, 0.1)};
+    private MkXboxInput climbInput = new MkXboxInput(op, ClimbInput.climbAxis, Type.Axis, false);
+    private MkXboxInput autoClimbInput = new MkXboxInput(op, ClimbInput.autoClimbButton, Type.Button, true);
+    private MkXboxInput elevatorInput = new MkXboxInput(op, ElevatorInput.elevatorAxis, Type.Axis, false, 0.1);
     private double pov = xbox.getPOV();
     private SwerveAlgorithims swerve = SwerveAlgorithims.getInstance();
     private Shooter shoot = Shooter.getInstance();
     private Elevator elevator = Elevator.getInstance();
     private Climber climb = Climber.getInstance();
     private Intake intake = Intake.getInstance();
+    private double fwd, str, rcw;
 
     public static Input getInstance()
     {
@@ -94,9 +97,13 @@ public class Input {
             intake.setIntake(0, ControlMode.Position);
         }
 
-        if(Math.abs(rollerInput.getAxis()) > 0)
+        if(rollerInput[0].isPressed())
         {
-            intake.setRoller(rollerInput.getAxis() * INTAKE.rollerPercentSpeed, ControlMode.PercentOutput);
+            intake.setRoller(INTAKE.rollerPercentSpeed, ControlMode.PercentOutput);
+        }
+        else if(rollerInput[1].isPressed())
+        {
+            intake.setRoller(-INTAKE.rollerPercentSpeed, ControlMode.PercentOutput);
         }
         else  
         {
@@ -105,48 +112,62 @@ public class Input {
     }
 
     public void climb()
-    {
-        if(climberLeftInput[0].isPressed())
+    {/*
+        if(pov == ClimbInput.leftClimbUpPOV)
         {
             climb.setLeftClimb(CLIMBER.climbUpSpeed, ControlMode.PercentOutput);
         }
-        else if(climberLeftInput[1].isPressed())
+        else if(pov == ClimbInput.leftClimbDownPOV)
         {
             climb.setLeftClimb(CLIMBER.climbDownSpeed, ControlMode.PercentOutput);
         }
 
-        if(climberRightInput[0].isPressed())
+        if(pov == ClimbInput.rightClimbUpPOV)
         {
             climb.setRightClimb(CLIMBER.climbUpSpeed, ControlMode.PercentOutput);
         }
-        else if(climberRightInput[1].isPressed())
+        else if(pov == ClimbInput.rightClimbDownPOV)
         {
             climb.setRightClimb(CLIMBER.climbDownSpeed, ControlMode.PercentOutput);
         }
 
-        if(Math.abs(climbInput.getAxis()) > 0)
+        if(Math.abs(climbInput.getAxis()) > 0.1)
         {
-            climb.setClimb(Math.abs(climbInput.getAxis()) * (climbInput.getAxis() < 0 ? CLIMBER.climbDownSpeed : CLIMBER.climbUpSpeed), ControlMode.PercentOutput);
+            climb.setClimb(Math.abs(climbInput.getAxis()) * (climbInput.getAxis() < 0.1 ? CLIMBER.climbDownSpeed : CLIMBER.climbUpSpeed), ControlMode.PercentOutput);
         }
 
-        if(!climberLeftInput[0].isPressed() && !climberLeftInput[1].isPressed() && !climberRightInput[0].isPressed() && !climberRightInput[1].isPressed() && Math.abs(climbInput.getAxis()) < climbInput.getThreshold())
+        if(pov != ClimbInput.leftClimbUpPOV && pov != ClimbInput.leftClimbDownPOV && pov != ClimbInput.rightClimbUpPOV && pov != ClimbInput.rightClimbDownPOV && Math.abs(climbInput.getAxis()) < 0.1)
         {
             climb.setClimb(0, ControlMode.PercentOutput);
-        }
+        }*/
+        SmartDashboard.putNumber("pov", pov);
     }
 
     public void drive()
     {
-        swerve.etherSwerve(driveInput[0].getAxis(), -driveInput[1].getAxis(), pov == 0 ? (fakeLimelightInput.isPressed() ? swerve.headerStraighter(Odometry.getInstance().getAngleFromGoal()) : driveInput[2].getAxis()) : swerve.headerStraighter(pov));
+        fwd = driveInput[0].getAxis();
+        str = -driveInput[1].getAxis();
+        rcw = (pov == -1) ? (fakeLimelightInput.isPressed() ? swerve.headerStraighter(Odometry.getInstance().getAngleFromGoal()) : driveInput[2].getAxis()) : swerve.headerStraighter(pov);
+        if(fwd != 0 || str != 0 || rcw != 0)
+        {
+            //MkSwerveTrain.getInstance().getModules()[0].turnMotor().setFalcon(ControlMode.Velocity, fwd * 90);
+            swerve.etherSwerve(fwd / MKTRAIN.speedLimit,str / MKTRAIN.speedLimit, rcw / MKTRAIN.speedLimit);
+        }
+        else
+        {
+            MkSwerveTrain.getInstance().stopEverything();
+        }
+        //swerve.etherSwerve(0, 0, 0);
+        //SmartDashboard.putNumber("axis", driveInput[1].getAxis());
     }
 
     public void mechanisims()
     {
         drive();
-        intake();
-        elevator();
-        shooter();
-        climb();
+        //intake();
+        //elevator();
+        //shooter();
+        //climb();
     }
 
     public double getPOV()
